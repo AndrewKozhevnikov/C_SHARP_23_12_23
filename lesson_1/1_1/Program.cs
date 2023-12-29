a7efe413cedc1a4e738d1024f1c0f840064535db
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2023-12-29 18:36:08 +0500

    Classwork task 1_3

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,10 +1,14 @@
-﻿int a = 25;
-int b = 5;
+﻿// Проверить, является первое число квадратом второго числа.
+
+int a = 25;
+int b = 5; 
 
 if (b * b == a)
     Console.WriteLine ("true");
 else
     Console.WriteLine ("false");  
+
+// Вывести квадрат числа.
 
 Console.Write("Enter a number: ");
 int num1 = int.Parse(Console.ReadLine()!);
