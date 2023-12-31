7ee4d02206135f674c968619289b2235fdab979a
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2023-12-31 14:07:50 +0500

    Classwork 2, task 3

---
+++ lesson_2/2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// задание 3. Написать программу, которая принимает на вход два числа
+// и выводит, является ли второе число кратным первому числу.
+// Если нет, то выводит остаток от деления.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int ostatok = a % b;
+if (ostatok == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет, " + ostatok);
+}
