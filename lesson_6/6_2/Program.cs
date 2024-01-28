790e5468090453d06e0928f0bf02683dc6ca5c7d
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-28 19:28:38 +0500

    Classwork 6: task 2

---
+++ lesson_6/6_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// На основе символов строки (тип string)
+// сформировать массив символов (тип char[]). Вывести массив на экран.
+
+char[] StrToCharArr(string str)
+{
+    int length = str.Length;
+    char[] arr = new char[length];
+    for (int i = 0; i < length; i++)
+        arr[i] = str[i];
+    return arr;
+}
+
+string str1 = Console.ReadLine();
+char[] arr1 = StrToCharArr(str1);
+Console.WriteLine(arr1);
+foreach (var item in arr1)
+{
+    Console.Write(item + ", ");
+}
