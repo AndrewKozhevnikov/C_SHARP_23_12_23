51d7df37eb31fe72ef20cd1d845784133790895a
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-28 18:44:05 +0500

    Classwork 6: task 1

---
+++ lesson_6/6_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Создайте массив символов типа char.
+// Создайте строку из символов этого массива.
+
+string Concat(char[] mass)
+{
+    string buf = "";
+    for (int i = 0; i < mass.Length; i++)
+        buf = buf + mass[i];
+    return buf;
+}
+
+char[] charArr = {'H', 'e', 'l', 'l', 'o'};
+string result = Concat(charArr);
+Console.WriteLine(result);
