1ea4747f0ae9c95ac7c6e19c091201c22b97fcc0
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-02-07 00:57:00 +0500

    Classwork 7: task 3

---
+++ lesson_7/7_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Написать программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// Использовать рекурсию.
+
+string str1 = "aeyiou";
+
+void Conson(string word, int i = 0)
+{
+    if (i == word.Length)
+        return;
+    if (str1.Contains(char.ToLower(word[i])) == false)
+        Console.Write(word[i] + " ");
+    Conson(word, i + 1);
+}
+
+Console.WriteLine("Enter letters: ");
+string str2 = Console.ReadLine()!;
+Conson(str2);
